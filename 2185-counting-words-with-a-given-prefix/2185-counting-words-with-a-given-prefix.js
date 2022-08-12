17dbeb6866703800b8fc384ef10d8c2773d36ebf
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-08-11 22:58:26 -0700

    Time: 123 ms (7.93%), Space: 47.7 MB (5.29%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+    let counter = 0;
+    const prefixLength = pref.length;
+    for(let i = 0; i < words.length; i++) {
+        console.log(words[i].substring(0, prefixLength));
+        if(words[i].substring(0, prefixLength) === pref) {
+            counter++;
+        }
+    }
+    return counter;
+};
